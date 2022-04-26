9c642b698ff15e174d0a12c0b9f4704c6e5bb64f
Kang Chiang <kachiangdev@gmail.com>
2022-04-25 23:44:11 -0700

    added redirect file

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./category-item.style.scss";
 
@@ -8,8 +9,10 @@ function CategoryItem({ category }) {
         <div className="category-container">
           <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}}/>
           <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
+            <Link to={`/shop/${title}`}>
+              <h2>{title}</h2>
+              <p>Shop Now</p>
+            </Link> 
           </div>
         </div>
     );
