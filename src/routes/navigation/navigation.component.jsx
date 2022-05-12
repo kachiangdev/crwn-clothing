32a1592b16ca5ad280525e4d3b9b6866958c5e4c
Kang Chiang <kachiangdev@gmail.com>
2022-05-12 13:24:34 -0700

    migrated from context to redux

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,17 +1,22 @@
-import { Fragment, useContext } from "react";
+import { Fragment } from "react";
 import { Outlet , Link} from "react-router-dom";
-import { UserContext } from "../../contexts/user.context";
-import { CartContext } from "../../contexts/cart.context";
+import { useSelector } from "react-redux";
+// import { UserContext } from "../../contexts/user.context";
+// import { CartContext } from "../../contexts/cart.context";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import { selectCurrentUser } from "../../store/user/user.selector";
+import { selectIsCartOpen } from "../../store/cart/cart.selector";
 import "./navigation.style.scss";
 
 
 function Navigation() {
-  const { currentUser } = useContext(UserContext);
-  const { isCartOpen } = useContext(CartContext);
+  // const { currentUser } = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser);
+
+  const isCartOpen = useSelector(selectIsCartOpen);
   const handleSignOutUser =  async() => {
     await signOutUser();
   }
